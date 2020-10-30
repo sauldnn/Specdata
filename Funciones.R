5201272dc37e9c7495ef3ab7cf98abd45cc12013
sauldnn <saul.dnn7@gmail.com>
2020-10-30 01:55:03 -0600

    funcion complete

--- Funciones.R
+++ Funciones.R
@@ -1,6 +1,6 @@
 pollutantmean <- function(directory, pollutant, id) {
   permonitor<-matrix(nrow = length(id), ncol = 2)
-  contador=1
+  contador<-1
   for(i in id) {
     libro<-read.csv(paste(directory, "/" , id, ".csv", sep=""), header=TRUE)
     Agente<-libro$pollutant
@@ -11,4 +11,16 @@ pollutantmean <- function(directory, pollutant, id) {
   }
   Promesa<-sum(permonitor[,1])/sum(permonitor[,2])
 }
-  
+
+complete<-function(directory, id){
+  mediciones<-matrix(nrow = length(id), ncol = 2)
+  contador<-1
+  for (i in id) {
+    libro<-read.csv(paste(directory, "/" , id, ".csv", sep=""), header=TRUE)
+    good<-complete.cases(libro)
+    numero<-length(libro[good,][,1])
+    mediciones[contador, 2]<-nuymero
+    mediciones[contador, 1]<-id
+    contador<-contador+1
+  }
+}
